ce0cef5746cc24a9bbd3f915437dcdd914c8ca05
Omua123 <omuakmv@gmail.com>
2025-08-12 10:37:27 -0400

    commit

---
+++ Lesson 1/main.py
@@ -0,0 +1,11 @@
+# Printing a greeting message
+print("Hello, world!")
+
+# Printing my name
+print("My name is Omua.")
+
+# Printing a fun fact about myself
+print("My favourite colour is purple.")
+
+# Performing and printing a simple calculation
+print("The result of 3 + 5 is:", 3 + 5)
